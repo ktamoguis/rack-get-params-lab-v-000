cf50afe8ffe052f8f277f7a0036f8fa1d475cf16
Karl Amoguis <ktamoguis@gmail.com>
2018-06-08 03:11:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,18 +8,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-      
     elsif req.path.match(/search/)
-      
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      
     elsif req.path.match(/cart/)
-
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
@@ -27,13 +22,14 @@ class Application
           resp.write "#{cart_item}\n"
         end
       end
-      
     elsif req.path.match(/add/)
       add_item = req.params["item"]
 
       if @@items.include?(add_item)
-        
-      
+        resp.write "We added #{add_item} to your cart"
+      else
+        resp.write "We don't have that item"
+
     else
       resp.write "Path Not Found"
     end
